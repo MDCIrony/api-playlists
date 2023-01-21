48fd8cbafaa9ae965e9f149d2cbde3b08612c62f
MigueldCast <miguel.acastillodiaz@outlook.es>
2023-01-21 08:08:37 -0500

    Reestructurando carpetas, configurando TS y dependencias, mejorando script para ejecución e inicializando sv

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Proyecto: Express-playlist
+
+## Para iniciar:
+### 1- Instalar dependencias: ```npm install```
+### 2- Ejecutar sv ```npm run dev``
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import app from "./app"
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+
+
+//Importamos las variables de .env
+dotenv.config()
+
+//Instanciamos el puerto
+const PORT = process.env.PORT
+const APP_NAME = process.env.APP_NAME
+
+//Activamos el servidor
+app.listen(PORT, () => console.log(`Server init at http://localhost:${PORT}/${APP_NAME}`))
