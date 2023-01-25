d7153b9c61eb25c801f029baebb1a90ed9d926e2
monte <montenegrojds@gmail.com>
2023-01-24 23:02:10 -0500

    remove due erros

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,6 @@ dotenv.config()
 
 //Instanciamos el puerto
 const PORT = process.env.PORT
-const APP_NAME = process.env.APP_NAME
 
 //Activamos el servidor
 app.listen(PORT, () => console.log(`Server init at http://localhost:${PORT}/api/v1/`))
